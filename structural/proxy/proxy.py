ae3d8f7926099382e1da47852963e71d61209705
Victor Hugo <victorhugocordeirocha@gmail.com>
2025-09-30 14:44:13 -0400

    Proxy

---
+++ structural/proxy/proxy.py
@@ -0,0 +1,90 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from time import sleep
+from typing import List, Dict
+
+
+class IUsuario(ABC):
+    """ Subject Interface """
+
+    @abstractmethod
+    def pega_endereco(self) -> List[Dict]: pass
+
+    @abstractmethod
+    def pega_dados_usuarios(self) -> Dict: pass
+
+class UsuarioReal(IUsuario):
+    """ Real Subject """
+
+    def __init__(self, primeiro_nome: str, segundo_nome: str) -> None:
+        sleep(2) # Simulando uma requisição
+        self.primeiro_nome = primeiro_nome
+        self.segundo_nome = segundo_nome
+
+
+    def pega_endereco(self) -> List[Dict]: 
+        sleep(2) # Simulando uma requisição
+        return [
+            {'avenida': 'brasil', 'numero': '01010'}
+        ]
+
+    def pega_dados_usuarios(self) -> Dict: 
+        sleep(2) # Simulando uma requisição
+        return {
+            'cpf': '111.111.111-11',
+            'rg': 'AB123456'
+        }
+    
+class Proxy(IUsuario):
+    """ Proxy """
+    def __init__(self, primeiro_nome: str, segundo_nome: str) -> None:
+        self.primeiro_nome = primeiro_nome
+        self.segundo_nome = segundo_nome
+
+        # Esses objetos nesse ponto do código ainda não existem
+        self._usuario_real: UsuarioReal
+        self._cache_endereco: List[Dict]
+        self._dados_usuarios: Dict
+
+    def pega_usuario_real(self) -> None:
+        if not hasattr(self, '_usuario_real'):
+            self._usuario_real = UsuarioReal(self.primeiro_nome, self.segundo_nome)
+
+    def pega_endereco(self) -> List[Dict]: 
+        self.pega_usuario_real()
+
+        if not hasattr(self, '_cache_endereco'):    
+            self._cache_endereco = self._usuario_real.pega_endereco()
+        
+        return self._cache_endereco
+        
+    def pega_dados_usuarios(self) -> Dict: 
+        self.pega_usuario_real()
+
+        if not hasattr(self, '_dados_usuarios'):    
+            self._dados_usuarios = self._usuario_real.pega_dados_usuarios()
+        
+        return self._dados_usuarios
+
+if __name__ == '__main__':
+    usuario1 = Proxy('Victor', 'Hugo')
+
+    # Responde instantaneamente
+    print(usuario1.primeiro_nome)
+    print(usuario1.segundo_nome)
+
+    # 6 segundos para obter resposta
+    print(usuario1.pega_dados_usuarios())
+    print(usuario1.pega_endereco())
+
+    # Aqui é respondido instantaneamente
+    print('DADOS EM CACHE')
+    for i in range(5):
+        print(usuario1.pega_endereco())
+        print(usuario1.pega_dados_usuarios())
+
+    """
+    O Proxy vai ter os mesmos metodos do objeto real. Porque ele implementa
+    a mesma interface. O proxy vai manter uma ligação com o objeto real,
+    "Ele conhece o objeto", e pode manter o objeto real dentro do seu codigo. 
+    """
